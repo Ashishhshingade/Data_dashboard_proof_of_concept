490deebbac988124c1caf2a18ded48587ee9b7c4
Ashish Shingade <66113291+Ashishhshingade@users.noreply.github.com>
2022-07-19 13:28:06 +0530

    render mysql - html

---
+++ data_viz/templates/charts.js
@@ -0,0 +1,128 @@
+const ctx = document.getElementById('myChart').getContext('2d');
+const chart2 = document.getElementById('myChart2').getContext('2d');
+const chart3 = document.getElementById('myChart3').getContext('2d');
+const chart4 = document.getElementById('myChart4').getContext('2d');
+
+const myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        responsive: false,
+    }
+});
+
+const myChart2 = new Chart(chart2, {
+    type: 'pie',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        responsive: false,
+    }
+});
+
+const myChart3 = new Chart(chart3, {
+    type: 'bar',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        responsive: false,
+    }
+});
+
+const myChart4 = new Chart(chart4, {
+    type: 'line',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        responsive: false,
+    }
+});
--- data_viz/views.py
+++ data_viz/views.py
@@ -1,21 +1,33 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
+from json import dumps
 # Create your views here.
 import mysql.connector
 
-def data_fetch(request):
-     conn = mysql.connector.connect(user='ashish', password='ashish@123',
-                               host='Desktop-G3', database='sql_store', buffered=True)
-     cursor = conn.cursor()
 
-     cursor.execute('select * FROM customers')
-     my_result = cursor.fetchall()
-     for x in my_result:
-          print(x)
-     return HttpResponse('hello')
+def data_fetch(request):
+    conn = mysql.connector.connect(user='ashish', password='ashish@123',
+                                   host='Desktop-G3', database='sql_store', buffered=True)
+    cursor = conn.cursor()
+
+    cursor.execute('select customer_id FROM customers')
+    my_result = cursor.fetchall()
+    # print(type((my_result)))
+    a = []
+    for x in my_result:
+        a.append(x)
+        # print(a)
+
+    out = [item for t in a for item in t]
+    print(out)
+    dataJSON = dumps(out)
+    return render(request, 'index.html', context={'data': dataJSON})
 
 
 def say_hello(request):
-     # return render(request,'hello.html')
-     return HttpResponse('hello')
+    return render(request, 'hello.html', context={
+        "brand": "Ford",
+        "model": "Mustang",
+        "year": 1964
+    },)
+    # return HttpResponse('hello')
